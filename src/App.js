62dbb9a0f0355c95232d6b7065d8cf05b7b953a4
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-06-30 20:53:59 +0600

    setup complete

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navabar from './Components/Navabar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navabar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navabar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navabar = () => {
+    return (
+        <div class="navbar bg-base-300">
+            <span class="btn btn-ghost normal-case text-xl">Power Hacker</span>
+            <button class="btn btn-primary ml-auto">Logout</button>
+        </div>
+    );
+};
+
+export default Navabar;
