00f014e9af6d5603f950ce99721c00c77b71dc41
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-07-01 14:45:43 +0600

    minor bug fixed

--- src/Components/AllButtons.js
+++ src/Components/AllButtons.js
@@ -4,7 +4,7 @@ import PaginationBtn from './PaginationBtn';
 
 const AllButtons = () => {
     // extracting states from context
-    const { pageCountArr, loading, currentPage, setCurrentPage } = useContext(AppContext)
+    const { pageCountArr, loading, currentPage, setCurrentPage, pageData } = useContext(AppContext)
     // simple loading text while updating something
     if (loading) {
         return <p>Loading...</p>
@@ -13,10 +13,10 @@ const AllButtons = () => {
     return (
         <div className="flex w-3/5 mx-auto gap-2 flex-wrap my-8">
             {/* if current page is 1 so we can surely tell that user cannot go to previous page */}
-            <button className="btn btn-success" disabled={currentPage === 1} onClick={() => setCurrentPage((pre) => pre - 1)}>&#8592;</button>
+            {pageData.length > 0 && <button className="btn btn-success" disabled={currentPage === 1} onClick={() => setCurrentPage((pre) => pre - 1)}>&#8592;</button>}
             {pageCountArr.map((page) => <PaginationBtn key={page} currentPage={currentPage} setCurrentPage={setCurrentPage}>{page}</PaginationBtn>)}
             {/* if current page is the last page so we can surely tell that user cannot go to next page */}
-            <button className="btn btn-success" disabled={pageCountArr.length === currentPage} onClick={() => setCurrentPage((pre) => pre + 1)}>&#8594;</button>
+            {pageData.length > 0 && <button className="btn btn-success" disabled={pageCountArr.length === currentPage} onClick={() => setCurrentPage((pre) => pre + 1)}>&#8594;</button>}
         </div>
     );
 };
